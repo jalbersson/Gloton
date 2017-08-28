2f15b6af96a16191b3092a3977c78a98eeebf20c
Daniel <dcepeda@unicauca.edu.co>
2017-08-28 15:22:47 -0500

    imagenes

--- app/src/main/java/onsite/gloton/com/co/gloton/Splash.java
+++ app/src/main/java/onsite/gloton/com/co/gloton/Splash.java
@@ -8,13 +8,13 @@ import android.location.LocationManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
-
+/*
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
-
+*/
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
